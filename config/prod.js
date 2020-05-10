b3d648f443f5a45b022d5cb10e55c8c7f7c17aa7
Richard Zhang <richard_zhang12@yahoo.com>
2020-05-09 19:03:16 -0500

    push changes to heroku

---
+++ config/prod.js
@@ -0,0 +1,5 @@
+module.exports = {
+    MongoDB: 'mongodb+srv://onlinedatingapp:Wanglin12@onlinedatingapp-otcyj.mongodb.net/test?retryWrites=true&w=majority',
+    FacebookAppID: '670075133753941',
+    FacebookAppSecret: 'b08a056e58343c1292cd146e34a54e21'
+};
