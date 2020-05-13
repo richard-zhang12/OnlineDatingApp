d62ea35f0cc3ac2c2c03a1e7573ae8e7be4fddf8
Richard Zhang <richard_zhang12@yahoo.com>
2020-05-13 05:31:25 -0500

    aws id and secret added

--- config/prod.js
+++ config/prod.js
@@ -1,5 +1,7 @@
 module.exports = {
     MongoDB: process.env.DB_MONGO,
     FacebookAppID: '670075133753941',
-    FacebookAppSecret: 'b08a056e58343c1292cd146e34a54e21'
+    FacebookAppSecret: 'b08a056e58343c1292cd146e34a54e21',
+    AWSAccessKeyID: process.env.AWS_ACCESS_KEY, 
+    AWSAccessKeySecret: process.env.AWS_ACCESS_SECRET
 };
